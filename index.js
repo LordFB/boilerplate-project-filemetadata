18f9578a641280a836b9bcad768270c96f8917f8
lordfb <67217617+LordFB@users.noreply.github.com>
2023-11-24 15:07:56 +0000

    Update 5 files

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer');;
+const upload = multer({ dest: '/public' });
+
 require('dotenv').config()
 
 var app = express();
@@ -11,10 +14,11 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.get('/api/fileanalyze', upload.single('upfile'), (req,res)=>{
+  console.log(req.file);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port ' + port);
 });
